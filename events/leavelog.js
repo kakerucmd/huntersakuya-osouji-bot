b88f675e0caf21c4d28160e70054d5b97e75540e
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2024-05-03 17:44:41 +0900

    Update leavelog.js

--- events/leavelog.js
+++ events/leavelog.js
@@ -15,7 +15,7 @@ module.exports = {
                 name: `${member.guild.name}`,
                 iconURL: `${member.guild.iconURL()}`
               })  
-            .setDescription(`**<@${member.username}>**(${member.id})さんが\n**${member.guild.name}**から退出しました`)
+            .setDescription(`**${member.user.username}**(${member.id})さんが\n**${member.guild.name}**から退出しました`)
             .setColor("Blurple")
             .setTimestamp();
             await channel.send({ embeds: [embed] }).catch(err => {});
