99a8f517a4b43044b9cf9c27de60237c3fcf64ff
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2024-05-03 17:05:15 +0900

    Update kaso.js

--- commands/kaso.js
+++ commands/kaso.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,11 +6,12 @@ module.exports = {
 		.setDescription('過疎化注意の画像を貼ります'),
 	async execute(interaction) {
 		try {
+			const file = new AttachmentBuilder('../images/kaso.png');
 			const embed = new EmbedBuilder()
 				.setColor('#FF0000')
 				.setTitle('サーバーが過疎化しています')
-				.setImage('https://media.discordapp.net/attachments/1160798192166776892/1163409629527679058/image.png?ex=66301266&is=662ec0e6&hm=7234104c5ce32316ccd951c4efe091b531215a8bd8041f8dabb177ff009b4798&=&format=webp&quality=lossless&width=1178&height=662');
-			await interaction.reply({ embeds: [embed] });
+				.setImage('attachment://kaso.png');
+			await interaction.reply({ embeds: [embed], files: [file] });
 		} catch (error) {
 			console.error(`エラーが発生しました: ${error}`);
 			await interaction.reply({ content: 'エラーが発生しました。後ほど再試行してください。', ephemeral: true });
