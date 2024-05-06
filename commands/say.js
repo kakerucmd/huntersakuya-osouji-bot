fa6f28a9d11528b040a629d0abba98626ff0343d
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2024-05-06 15:04:20 +0900

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,14 +1,25 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+    cooldown: 20,
     data: new SlashCommandBuilder()
         .setName('say')
-        .setDescription('入力したメッセージをお掃除上方修正しろbotに喋らせます'),
+        .setDescription('入力したメッセージをお掃除上方修正しろbotに喋らせます')
+        .addStringOption(option => 
+            option.setName('message')
+                .setDescription('メッセージを入力')
+                .setRequired(true)),
     async execute(interaction) {
-        const embed = new EmbedBuilder()
-            .setColor('#ff0000')
-            .setTitle('現在このコマンドは無効化されています。')
 
-        await interaction.reply({ embeds: [embed] , ephemeral: true });
+        const message = interaction.options.getString('message');
+
+        const containsToken = /([a-zA-Z0-9-_]{24}\.[a-zA-Z0-9-_]{6}\.[a-zA-Z0-9-_]{27})|mfa\.[a-z0-9_-]{20,}/i.test(message);
+
+        if (containsToken) {
+            await interaction.reply({ content: 'Token類似文字列を含むメッセージは送信できません。', ephemeral: true });
+            return;
+        }
+
+        await interaction.reply({ content: message });
     },
 };
