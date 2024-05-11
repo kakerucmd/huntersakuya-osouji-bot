7df5386019e2b7a8baa484a9ba750e84b72dfa5a
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2024-05-12 03:43:30 +0900

    Delete events directory

--- events/jointimestamp.js
+++
@@ -1,9 +0,0 @@
-const { Events } = require('discord.js');
-
-module.exports = {
-	name: Events.GuildMemberAdd,
-	once: false,
-	async execute(member) {
-        global.joinTimestamps.set(member.id, Date.now());
-	},
-};
