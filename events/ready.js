f57b240095cb1c507ff299260265083e40f10140
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2024-05-24 13:34:16 +0900

    Delete events directory

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-const { Events } = require('discord.js');
-
-module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		console.log(`お掃除上方修正しろ！！(${client.user.tag}が起動しました)`);
-	},
-};
