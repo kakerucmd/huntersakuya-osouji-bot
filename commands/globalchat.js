a3865b08119853c79bf7ae840ff0651138535bce
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2024-05-26 09:20:11 +0900

    Update globalchat.js

--- commands/globalchat.js
+++ commands/globalchat.js
@@ -14,7 +14,7 @@ module.exports = {
 
     async execute (interaction) {
 
-        const  { options } = interaction;
+        const { options } = interaction;
         const sub = options.getSubcommand();
 
         const channel = options.getChannel('channel');
@@ -23,6 +23,16 @@ module.exports = {
         switch (sub) {
             case 'enable':
                 await interaction.deferReply({ ephemeral: true });
+
+                if (channels[channel.id]) {
+                    const embed = new EmbedBuilder()
+                        .setAuthor({ name: '⚠️｜エラー' })
+                        .setDescription(`グローバルチャットは既に${channel}で有効化されています。`)
+                        .setColor('#ff0000');
+                    await interaction.editReply({ embeds: [embed] });
+                    return;
+                }
+
                 try {
                     const webhook = await channel.createWebhook({ name: "Global chat Webhook" });
                     channels[channel.id] = webhook.url;
@@ -31,39 +41,55 @@ module.exports = {
                         .setAuthor({ name: '✅｜成功' })
                         .setDescription(`グローバルチャットが${channel}で有効化されました。\nスパム行為などはおやめください。`)
                         .setColor('#00ff00');
-                    await interaction.editReply({ embeds: [embed], ephemeral: true });
+                    await interaction.editReply({ embeds: [embed] });
 
                     await channel.setRateLimitPerUser(5);
                 } catch (error) {
                     const embed = new EmbedBuilder()
                         .setAuthor({ name: '❌｜エラー' })
-                        .setDescription(`Webhookの作成中にエラーが発生しました。BotにWebhookを作成する権限があることを確認してください。`)
+                        .setDescription(`Webhookの作成中にエラーが発生しました。\nBotにWebhookを作成する権限があることを確認してください。`)
                         .setColor('#ff0000');
-                    return interaction.editReply({ embeds: [embed], ephemeral: true });
+                    return interaction.editReply({ embeds: [embed] });
                 }
                 await globalchannels.set('globalchannels', channels);
                 break;
 
-            case 'disable':
-                await interaction.deferReply({ ephemeral: true });
-                if (!channels[channel.id]) {
+                case 'disable':
+                    await interaction.deferReply({ ephemeral: true });
+                    if (!channels[channel.id]) {
+                        const embed = new EmbedBuilder()
+                            .setAuthor({ name: '⚠️｜エラー' })
+                            .setDescription(`グローバルチャットが${channel}で有効化されていません。`)
+                            .setColor('#ff0000');
+                        await interaction.editReply({ embeds: [embed] });
+                        return;
+                    }
+                
+                    const webhookURL = channels[channel.id];
+                    delete channels[channel.id];
+                    await globalchannels.set('globalchannels', channels);
+                
+                    try {
+                        const webhooks = await channel.fetchWebhooks();
+                        for (const webhook of webhooks.values()) {
+                            if (webhook.url === webhookURL) {
+                                await webhook.delete();
+                            }
+                        }
+                    } catch (error) {
+                        const embed = new EmbedBuilder()
+                            .setAuthor({ name: '⚠️｜成功' })
+                            .setDescription(`グローバルチャットの無効化に成功しましたが、グローバルチャット用Webhookの削除に失敗しました。\nグローバルチャット用Webhookは手動で削除を行ってください。`)
+                            .setColor('#ff0000');
+                        return interaction.editReply({ embeds: [embed] });
+                    }
+                
                     const embed = new EmbedBuilder()
-                        .setAuthor({ name: '❌｜エラー' })
-                        .setDescription(`グローバルチャットが${channel}で有効化されていません。`)
-                        .setColor('#ff0000');
-                    await interaction.editReply({ embeds: [embed], ephemeral: true });
-                    return;
-                }
-
-                delete channels[channel.id];
-                const embed = new EmbedBuilder()
-                    .setAuthor({ name: '✅｜成功' })
-                    .setDescription(`グローバルチャットが${channel}で無効化されました。\n${channel}のグローバルチャット用Webhookは削除して大丈夫です。`)
-                    .setColor('#00ff00');
-                await interaction.editReply({ embeds: [embed], ephemeral: true });
-                await channel.setRateLimitPerUser(0);
-
-                await globalchannels.set('globalchannels', channels);
+                        .setAuthor({ name: '✅｜成功' })
+                        .setDescription(`グローバルチャットが${channel}で無効化されました。`)
+                        .setColor('#00ff00');
+                    await interaction.editReply({ embeds: [embed] });
+                    await channel.setRateLimitPerUser(0);                
         }
     }
 };
