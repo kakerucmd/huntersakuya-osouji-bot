f31e083a5989eb1e9b2a4af0bf5ef9bc95c6f64d
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2024-06-01 20:28:54 +0900

    Update osoujireply.js

--- events/osoujireply.js
+++ events/osoujireply.js
@@ -1,7 +1,8 @@
-const { Events } = require('discord.js');
+const { Events, Collection } = require('discord.js');
 const Keyv = require('keyv');
+
 const osouzireply = new Keyv('sqlite://db.sqlite', { table: 'osouzireply' });
-let lastReplyTime = {};
+const lastReplyTime = new Collection();
 
 module.exports = {
 	name: Events.MessageCreate,
@@ -12,6 +13,8 @@ module.exports = {
         let triggerWord;
         if (message.content.includes('お掃除上方修正')) {
             triggerWord = 'お掃除上方修正';
+        } else if (message.content.includes('大掃除')) {
+            triggerWord = '大掃除';
         } else if (message.content.includes('お掃除')) {
             triggerWord = 'お掃除';
         } else if (message.content.includes('上方修正')) {
@@ -23,7 +26,7 @@ module.exports = {
                 const isEnabled = await osouzireply.get(message.guild.id);
                 if (isEnabled) {
                     const now = Date.now();
-                    const lastReply = lastReplyTime[message.guild.id];
+                    const lastReply = lastReplyTime.get(message.guild.id);
                     if (lastReply && now - lastReply < 10000) {
                         await message.channel.sendTyping();
                         setTimeout(async () => {
@@ -34,20 +37,28 @@ module.exports = {
                             }
                         }, 1000);
                     } else {
-                        const escapedTriggerWord = triggerWord.replace(/@everyone/g, "").replace(/@here/g, "");
+                        let replyMessage = '';
+                        if (triggerWord === 'お掃除上方修正') {
+                            replyMessage = 'しろ！！';
+                        } else if (triggerWord === '大掃除') {
+                            replyMessage = '大掃除...？お掃除...逆襲....お掃除逆襲しろ！！';
+                        } else {
+                            const escapedTriggerWord = triggerWord.replace(/@everyone/g, "").replace(/@here/g, "");
+                            replyMessage = `${escapedTriggerWord}...？上方修正....お掃除...お掃除上方修正しろ！！！！`;
+                        }
                         await message.channel.sendTyping();
                         setTimeout(async () => {
                             try {
-                                await message.reply({ content: `${escapedTriggerWord}...？上方修正....お掃除...お掃除上方修正しろ！！！！` });
+                                await message.reply({ content: replyMessage });
                             } catch (error) {
                                 console.log(error)
-                                await message.channel.send(`${escapedTriggerWord}...？上方修正....お掃除...お掃除上方修正しろ！！！！`);
+                                await message.channel.send(replyMessage);
                             }
                         }, 1000);
                     }
-                    lastReplyTime[message.guild.id] = now;
+                    lastReplyTime.set(message.guild.id, now);
                     setTimeout(() => {
-                        delete lastReplyTime[message.guild.id];
+                        lastReplyTime.delete(message.guild.id);
                     }, 10000);
                 }
             } catch (error) {
