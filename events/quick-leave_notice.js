1b68ec8da823d4430ae2004a8d168a8b17f7bc36
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2024-06-17 08:24:34 +0900

    埋め込みに変更

--- events/quick-leave_notice.js
+++ events/quick-leave_notice.js
@@ -1,27 +1,38 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder } = require('discord.js');
 const Keyv = require('keyv');
 const toggleleave = new Keyv('sqlite://db.sqlite', { table: 'toggleleave' });
 const leavechannel = new Keyv('sqlite://db.sqlite', { table: 'leaveChannel' });
 
 module.exports = {
-	name: Events.GuildMemberRemove,
-	once: false,
-	async execute(member) {
+    name: Events.GuildMemberRemove,
+    once: false,
+    async execute(member) {
         const joinTimestamp = global.joinTimestamps.get(member.id);
         const leaveTimestamp = Date.now();
         const diffMinutes = (leaveTimestamp - joinTimestamp) / 1000 / 60;
-    
+
         if (diffMinutes <= 10) {
             const isEnabled = await toggleleave.get(member.guild.id);
             if (isEnabled) {
                 const channelId = await leavechannel.get(member.guild.id);
                 const channel = member.guild.channels.cache.get(channelId);
                 if (channel) {
-                    channel.send(`${member.user}が即抜けしました`);
+                    const embed = new EmbedBuilder()
+                        .setAuthor({
+                            name: member.user.tag,
+                            iconURL: member.user.displayAvatarURL()
+                        })
+                        .setDescription(`**${member.user}**(${member.user.id})さんが即抜けしました`)
+                        .setColor('Blurple')
+                        .setFooter({
+                            text: `${Math.floor(diffMinutes)}分${Math.floor((diffMinutes % 1) * 60)}秒で即抜けしました`
+                        });
+
+                    channel.send({ embeds: [embed] });
                 }
             }
         }
-    
+
         global.joinTimestamps.delete(member.id);
-	},
+    },
 };
