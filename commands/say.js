9766dca442f51398c909feff3159f4dcd1c94c28
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2025-03-02 21:45:47 +0900

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -10,16 +10,21 @@ module.exports = {
                 .setDescription('メッセージを入力')
                 .setRequired(true)),
     async execute(interaction) {
-
         const message = interaction.options.getString('message');
+        const user = interaction.user; 
+
+        console.log(`User: ${user.username} (${user.id}) requested to send message: ${message}`);
 
         const containsToken = /([a-zA-Z0-9-_]{24}\.[a-zA-Z0-9-_]{6}\.[a-zA-Z0-9-_]{27})|mfa\.[a-z0-9_-]{20,}/i.test(message);
 
         if (containsToken) {
+            console.log(`Blocked message from ${user.username} (${user.id}) due to token-like content.`);
             await interaction.reply({ content: 'Token類似文字列を含むメッセージは送信できません。', ephemeral: true });
             return;
         }
-
-        await interaction.reply({ content: message });
+        
+        console.log(`Sending message to channel from ${user.username} (${user.id})...`);
+        await interaction.reply({ content: 'メッセージを送信しました。\n※サーバーにログは残ります。' });
+        await interaction.channel.send({ content: message });
     },
 };
