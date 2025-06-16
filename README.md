a2df49c59f23757e52e40d0783754322bfb6f0b2
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2025-06-16 22:33:30 +0900

    Ver4.7

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 ```json
 {
   "bot_name": "お掃除上方修正しろbot#6554",
-  "version": "Ver4.5",
+  "version": "Ver4.7",
   "language": "Javascript",
   "description": "ネタで作り始めた多機能bot",
   "site": "https://kakerucmd.github.io/huntersakuya_osouji-bot/"
--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ module.exports = {
 		console.log(`お掃除上方修正しろ！！(${client.user.tag}が起動しました)`);
 
 		setInterval(() => {
-			client.user.setActivity(`Ver4.6 | ${client.guilds.cache.size} servers ${client.ws.ping}ms`);
+			client.user.setActivity(`Ver4.7 | ${client.guilds.cache.size} servers ${client.ws.ping}ms`);
 		}, 60000);
 	},
 };
